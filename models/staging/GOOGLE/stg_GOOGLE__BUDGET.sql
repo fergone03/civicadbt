b66637e742c9b7fe51888fddb5d0fa318f0085a1
Esteban Fernández <fergonesteban03@gmail.com>
2026-04-29 16:58:14 +0000

    k

--- models/staging/GOOGLE/stg_GOOGLE__BUDGET.sql
+++ models/staging/GOOGLE/stg_GOOGLE__BUDGET.sql
@@ -1,8 +1,18 @@
-with 
+{{ config(
+    materialized='incremental',
+    unique_key='product_id',
+) }}
 
-source as (
+with source as (
 
-    select * from {{ source('GOOGLE', 'BUDGET') }}
+    select * 
+    from {{ source('GOOGLE', 'BUDGET') }}
+    
+    {% if is_incremental() %}
+    where _fivetran_synced > (
+        select max(_fivetran_synced) from {{ this }}
+    )
+    {% endif %}
 
 ),
 
@@ -14,9 +24,9 @@ renamed as (
         month,
         product_id,
         _fivetran_synced
-
     from source
 
 )
+
 
 select * from renamed

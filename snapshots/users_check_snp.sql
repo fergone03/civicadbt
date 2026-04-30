8a7353191af57833563fb24b513aa2d6dffa60f0
Esteban Fernández <fergonesteban03@gmail.com>
2026-04-30 16:53:51 +0000

    snaps

---
+++ snapshots/users_check_snp.sql
@@ -0,0 +1,21 @@
+{% snapshot users_check_snp%}
+
+{{
+    config(
+        target_schema='snapshots',
+        unique_key='DNI',
+        strategy='check',
+        check_cols=['Nombre', 'email'],
+        hard_deletes='new_record'
+
+    )
+}}
+
+SELECT
+    Nombre,
+    DNI,
+    email,
+    fecha_alta_sistema
+FROM {{ source('GOOGLE', 'USERS') }}
+
+{% endsnapshot %}
